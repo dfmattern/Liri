16550102db0963b753da630b254c330bef0d1789
Dane <56905214+dfmattern@users.noreply.github.com>
2020-02-17 00:26:10 -0700

    installed axios, scaffolded functions

--- liri.js
+++ liri.js
@@ -3,3 +3,27 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+//console.log(process.argv);
+
+fucntion concertThis(){
+
+
+};
+
+function spotifyThis(){
+
+}
+
+function movieThis(){
+
+}
+
+function dwis (){
+    fs.readFile('random.txt', "utf8", function(error,data){
+        let txt = data.split(',');
+
+        spotifyThis(txt[1]);
+    })
+}
+
