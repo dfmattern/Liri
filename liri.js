c5bf95b14b27e2188d992f612a6f11185790a47e
Dane <56905214+dfmattern@users.noreply.github.com>
2020-02-17 22:50:18 -0700

    created concert-this function

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 let keys = require("./keys.js");
 
-let spotify = new Spotify(keys.spotify);
+//let spotify = new Spotify(keys.spotify);
 
 let axios = require("axios");
 
@@ -10,38 +10,46 @@ let moment = require("moment");
 
 let fs = require("fs");
 
-let Spotify = require('node-spotify-api');
-
-
+//let Spotify = require("node-spotify-api");
 
 //console.log(process.argv);
 
-function concertThis(){
-axios
-.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-.then(function(response){
-    console.log("Venue name:", + response.data);
-    //console.log("Venue location:", + response.data);
-    
-    
-})
+let command = process.argv[2];
+let userInput = process.argv[3];
 
-};
+function concertThis() {
+  let artist = userInput;
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        artist +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function(response) {
+      for (let i = 0; i < response.data.length; i++) {
+        let name = response.data[i].venue.name;
+        console.log("Venue name: " + response.data[i].venue.name);
+        let city = response.data[i].venue.city;
+        console.log("Venue location: " + response.data[i].venue.city);
+
+        let date = moment(response.data[i].datetime).format("MM/DD/YYYY");
+        console.log("Event date:" + date);
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
 concertThis();
 
-function spotifyThis(){
+function spotifyThis() {}
 
+function movieThis() {}
+
+function dwis() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    let txt = data.split(",");
+
+    spotifyThis(txt[1]);
+  });
 }
-
-function movieThis(){
-
-}
-
-function dwis (){
-    fs.readFile('random.txt', "utf8", function(error,data){
-        let txt = data.split(',');
-
-        spotifyThis(txt[1]);
-    })
-}
-
